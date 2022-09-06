c4b781a3b58520f6c9255bca059ed46859c02525
Maria Luiza Ribeiro Teixeira <maluribeiro08@gmail.com>
2022-09-06 09:16:33 -0300

    feat: ensure request body to be parsed as json

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,10 @@
 import express from 'express'
 
+// Express package instantiation
 const app = express()
+
+// App configuration
+app.use(express.json()) // Parse body to json to deal with it
+
+// Server initialization
 app.listen(5050, () => console.log('Server runing at http://localhost:5050'))
